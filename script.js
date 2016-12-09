da744dd00b13287d65e7775652db7ac63f73fa93
katya20 <kpanchenko21@gmail.com>
2016-12-09 08:39:04 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function polygon(distance, n) {
   }
 }
 
-//polygon(100,10);
+polygon(20,360);
 
 function star(distance) {
   for (i = 0; i < 5; i++){
@@ -17,33 +17,33 @@ function star(distance) {
   }
 }
 
-manyStars(100, 200, 200)
+//manyStars(100, 200, 200)
+//
+//function manyStars(distance) {
+//  for (size = distance; size > 0; size = size - 20) {
+//    star(size);
+//    var x = x + 25;
+//    var y = y - 20;
+//    var position = {x: x, y: y};
+//  }
+//}
+star (100);
 
-function manyStars(distance) {
-  for (size = distance; size > 0; size = size - 20) {
-    star(size);
-    var x = x + 25;
-    var y = y - 20;
-    
-  }
-}
-//star (100);
-//
-//var position = {x: 225, y: 180};
-//
-//star(80);
-//
-//var position = {x: 250, y: 160};
-//
-//star(60);
-//
-//var position = {x: 275, y: 140};
-//
-//star(40);
-//
-//var position = {x: 300, y: 120};
-//
-//star(20);
+var position = {x: 225, y: 180};
+
+star(80);
+
+var position = {x: 250, y: 160};
+
+star(60);
+
+var position = {x: 275, y: 140};
+
+star(40);
+
+var position = {x: 300, y: 120};
+
+star(20);
 
 
 
